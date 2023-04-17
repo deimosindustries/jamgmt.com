5172578938a13ef7dd4539a4903bf8b18f676d98
Luigi Mannoni <luigimannoni@users.noreply.github.com>
2023-04-17 22:52:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         </video>
       </div>
       <div className="w-full flex justify-center items-center">
-        <div className="w-full px-4 md:px-10 max-w-4xl animate-fade-in text-lg md:text-xl leading-relaxed -mt-[14rem] md:-mt-[20rem] md:-translate-y-10 relative z-10">
+        <div className="w-full px-4 md:px-10 max-w-4xl animate-fade-in text-lg md:text-xl leading-relaxed -mt-[14rem] md:-mt-[18rem] relative z-10">
           <p className="text-white text-center py-3">
             JAM is a full-service talent management company representing clients across entertainment, sports, fashion, and music. Founded in 2018, JAM currently has offices in Los Angeles, London, and Paris.
           </p>
