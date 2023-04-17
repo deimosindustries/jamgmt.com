858574596a10c1b744abde5e9e455b5f56790d99
Luigi Mannoni <hello@luigimannoni.com>
2023-04-17 20:13:41 +0100

    Centered

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,18 @@ function App() {
           <source src="./jam-logo-square.webm" type="video/webm" />
         </video>
       </div>
-      <div className="w-full px-4 md:px-10 max-w-4xl animate-fade-in text-lg md:text-xl leading-relaxed -mt-[14rem] md:-mt-[20rem] relative z-10">
-        <p className="text-white text-center py-3">
-          JAM is a full-service talent management company representing clients across entertainment, sports, fashion, and music. Founded in 2018, JAM currently has offices in Los Angeles, London, and Paris.
-        </p>
+      <div className="w-full flex justify-center items-center">
+        <div className="w-full px-4 md:px-10 max-w-4xl animate-fade-in text-lg md:text-xl leading-relaxed -mt-[14rem] md:-mt-[20rem] md:-translate-y-10 relative z-10">
+          <p className="text-white text-center py-3">
+            JAM is a full-service talent management company representing clients across entertainment, sports, fashion, and music. Founded in 2018, JAM currently has offices in Los Angeles, London, and Paris.
+          </p>
 
-        <p className="text-white text-center py-3">
-          <a href="mailto:info@jamgmt.com">info@jamgmt.com</a>
-        </p>
+          <p className="text-white text-center py-3">
+            <a href="mailto:info@jamgmt.com">info@jamgmt.com</a>
+          </p>
+        </div>
       </div>
+
     </div>
   );
 }
